ccd1ce65418b125fb700b7bc2d38f73ea4571f80
Aryobt <aryobt2@gmail.com>
2025-09-13 16:05:23 -0400

    added the task2 code but still would need some work

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
@@ -8,10 +9,12 @@ public class Player : MonoBehaviour
     public List<Transform> asteroidTransforms;
     public float bombTrailSpacing;
     public int numberOfTrailBombs;
+    public float inDistance = 1f;
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.B))
         {
             SpawnBombAtOffset (new Vector3(0,1));
@@ -22,6 +25,8 @@ public class Player : MonoBehaviour
             SpawnBombTrail (new Vector3(0,1,0));
 
         }
+
+
     }
 
     public void SpawnBombAtOffset (Vector3 inOffset)
@@ -35,4 +40,16 @@ public class Player : MonoBehaviour
         Vector3 spawnPosition = transform.position - inOffset;
       Instantiate(bombPrefab,spawnPosition, Quaternion.identity);
    }
+    public void SpawnBombOnRandomCorner ( Vector3 inOffset)
+    {
+        Vector3[] directions = new Vector3[]
+            {
+            new Vector3(-1, 0, 1),
+            new Vector3(1, 0, 1),
+            new Vector3(-1, 0, -1),
+            };
+        Vector3 randomDirection = directions[Random.Range(0, directions.Length)];
+        Vector3 spawnPos = transform.position + randomDirection.normalized * inDistance;
+        Instantiate(bombPrefab, spawnPos, Quaternion.identity);
+    }
 }
